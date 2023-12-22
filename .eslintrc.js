06d12459687a6267be6b36c3f6739c05522bdbda
mvss01 <marcosvictorss@hotmail.com>
2023-12-22 18:17:49 -0300

    V0.0.1

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        browser: true,
+        es6: true,
+        jest: true,
+    },
+    extends: ['airbnb', 'prettier'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'react-hooks', 'testing-library', 'prettier'],
+    rules: {
+        'prettier/prettier': 'warn',
+        'react/jsx-filename-extension': [
+            'warn',
+            { extensions: ['.jsx', '.js'] },
+        ],
+        'import/extensions': 'off',
+        'import/prefer-default-export': 'off',
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                packageDir: './',
+            },
+        ],
+        'react/function-component-definition': [
+            'error',
+            {
+                namedComponents: 'arrow-function',
+                unnamedComponents: 'arrow-function',
+            },
+        ],
+    },
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState, React } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const ESCAPE_KEY = 27;
+    const ENTER_KEY = 13;
+
+    const [value, setValue] = useState();
+
+    const erase = () => {
+        setValue('');
+    };
+
+    const submit = () => {
+        console.log('submit', value);
+        erase();
+    };
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const onKeyDown = (event) => {
+        if (event.which === ENTER_KEY) {
+            submit();
+        } else if (event.which === ESCAPE_KEY) {
+            erase();
+        }
+    };
+    return (
+        <section id="app" className="container">
+            <header>
+                <h1 className="title">To-Do</h1>
+            </header>
+            <section className="main">
+                <input
+                    className="new-todo"
+                    placeholder="o que precisa ser feio?"
+                    value={value}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                />
+            </section>
+        </section>
+    );
+};
 
 export default App;
